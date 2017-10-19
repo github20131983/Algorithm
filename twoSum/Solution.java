404d9f13ab98754fd5864c9a2d3df229a6b9a24f
github20131983 <32866135+github20131983@users.noreply.github.com>
2017-10-19 22:37:25 +0800

    1
    easy

---
+++ twoSum/Solution.java
@@ -0,0 +1,26 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+//map<key,value>
+public class Solution {
+  public int[] twoSum(int[] nums, int target){
+	  int result[]=new int[2];
+	  Map<Integer,Integer>  map=new HashMap<Integer,Integer>();
+	  for(int i=0;i<nums.length;i++){
+		  if(map.containsKey(target-nums[i])){
+			  result[0]=map.get(target-nums[i]);
+			  result[1]=i;
+			  return result;
+		  }
+		  map.put(nums[i],i);
+	  }
+	return result;	  
+  }
+  public static void main(String[] args){
+	  Solution s=new Solution();
+	  int a[]={2,7,11,5};
+	  for(int i=0;i<s.twoSum(a, 7).length;i++)
+	  System.out.println(s.twoSum(a, 7)[i]);
+  }
+}
