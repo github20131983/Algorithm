c4554c9f1a1a58395f56a9560f2e55ec77f35d82
github20131983 <32866135+github20131983@users.noreply.github.com>
2017-10-26 23:05:24 +0800

    14
    easy

---
+++ longestCommonPrefix/Solution.java
@@ -0,0 +1,23 @@
+package longestCommonPrefix;
+
+public class Solution {
+public String longestCommonPrefix(String[] strs) {
+	    if(strs==null||strs.length==0)return "";
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<strs[0].length();i++){
+        	char c=strs[0].charAt(i);
+        	for(int j=1;j<strs.length;j++){
+        		if(i>=strs[j].length()||strs[j].charAt(i)!=c)
+        			return res.toString();
+        	}
+        	res.append(c);
+        }
+        return res.toString();
+    }
+
+  public static void main(String[] args){
+	  Solution s=new Solution();
+	  String[] str={"aa","a"};
+	  System.out.println(s.longestCommonPrefix(str));
+  }
+}
