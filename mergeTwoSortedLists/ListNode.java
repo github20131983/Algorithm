772d8178a435824bade3445e0b6aa4b7f391a0f8
github20131983 <32866135+github20131983@users.noreply.github.com>
2017-10-28 18:19:07 +0800

    21
    easy

---
+++ mergeTwoSortedLists/ListNode.java
@@ -0,0 +1,7 @@
+package mergeTwoSortedLists;
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x){val=x;}
+}
---
+++ mergeTwoSortedLists/Solution.java
@@ -0,0 +1,44 @@
+package mergeTwoSortedLists;
+//µÝ¹é½â¾ö
+public class Solution {
+public ListNode mergeTwoSorterLists(ListNode l1,ListNode l2){
+	if(l1==null)
+		return l2;
+	if(l2==null)
+		return l1;
+    if(l1.val>l2.val){
+    	ListNode tmp=l2;
+    	tmp.next=mergeTwoSorterLists(l1,l2.next);
+    	return tmp;
+    }
+    else{
+    	ListNode tmp=l1;
+    	tmp.next=mergeTwoSorterLists(l1.next,l2);
+    	return tmp;
+    }
+}
+  public static void main(String[] args){
+	  ListNode l1[] = new ListNode[3];
+	  ListNode l2[] = new ListNode[3];
+	  l1[0]=new ListNode(2);
+	  l1[1]=new ListNode(4);
+	  l1[2]=new ListNode(6);
+	  
+	  l1[0].next=l1[1];
+	  l1[1].next=l1[2];
+	  l1[2].next=null;
+	  
+	  l2[0]=new ListNode(1);
+	  l2[1]=new ListNode(3);
+	  l2[2]=new ListNode(5);
+	  l2[0].next=l2[1];
+	  l2[1].next=l2[2];
+	  l2[2].next=null;
+	  Solution s=new Solution();
+	  ListNode ls=s.mergeTwoSorterLists(l1[0], l2[0]);
+	  while(ls!=null){
+		  System.out.println(ls.val);
+		  ls=ls.next;
+	  }
+  }
+}
