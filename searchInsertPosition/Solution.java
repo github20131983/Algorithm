a17a42dfb5092953fca33e90b7fb4463a3bbaf7e
github20131983 <32866135+github20131983@users.noreply.github.com>
2017-11-08 14:23:28 +0800

    35
    easy

--- searchInsertPosition/Solution.java
+++ searchInsertPosition/Solution.java
@@ -1,31 +1,25 @@
 package searchInsertPosition;
 
 public class Solution {
-public int romanToInt(String s) {
-        int araNu=0;
-        for(int i=s.length()-1;i>=0;i--){
-        	char single=s.charAt(i);
-        	switch(single){
-        	case 'I':
-        		araNu+=araNu>=5?-1:1;break;
-        	case 'V':
-        		araNu+=5;break;
-        	case 'X':
-        		araNu+=araNu>=50?-10:10;break;
-        	case 'L':
-        		araNu+=50;break;
-        	case 'C':
-        		araNu+=araNu>=500?-100:100;break;
-        	case 'D':
-        		araNu+=500;break;
-        	case 'M':
-        		araNu+=1000;break;    
-		}
-	}
-	return araNu;
-    }
+	 public int searchInsert(int[] nums, int target) {
+	       int left=0;
+	        int right=nums.length-1;
+	        int middle;
+	        while(right>=left){
+	        	middle=left+((right-left)>>1);
+	        	if(nums[middle]==target)
+	        		return middle;
+	        	else if(nums[middle]<target)
+	        		left=middle+1;
+	        	else
+	        		right=middle-1;
+	        }
+		return left;  
+	    }
   public static void main(String[] args){
 	 Solution s=new Solution();
-	 System.out.println(s.romanToInt("XCVIII"));
+	 int[] a={1,3,5,6};
+	 int b=5;
+	 System.out.println(s.searchInsert(a, b));
   }
 }
