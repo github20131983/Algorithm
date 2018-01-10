d5fd0dc6e7ac889ad2b82c3b441b3d5cdcf2f597
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-01-10 22:44:52 +0800

    104
    easy

---
+++ maximumDepthofBinaryTree/Solution.java
@@ -0,0 +1,31 @@
+package maximumDepthofBinaryTree;
+
+import maximumDepthofBinaryTree.TreeNode;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+        	return 0;
+    	int maxL=maxDepth(root.left);
+    	int maxR=maxDepth(root.right);
+    	return maxL>maxR? maxL+1:maxR+1;
+    }
+    public static void main(String[] args){
+	 TreeNode[] tr1=new TreeNode[7];
+   	 tr1[0]=new TreeNode(1);
+   	 tr1[1]=new TreeNode(2);
+   	 tr1[2]=new TreeNode(2);
+   	 tr1[3]=new TreeNode(3);
+   	 tr1[4]=new TreeNode(4);
+   	 tr1[5]=new TreeNode(4);
+   	 tr1[6]=new TreeNode(3);
+   	 tr1[0].left=tr1[1];
+   	 tr1[0].right=tr1[2];
+   	 tr1[1].left=tr1[3];
+   	 tr1[1].right=tr1[4];
+   	 tr1[2].left=tr1[6];
+   	 tr1[2].right=tr1[5];
+   	 Solution s=new Solution();
+   	 System.out.println(s.maxDepth(tr1[0]));
+}
+}
