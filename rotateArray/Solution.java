378fb925875c48eb28769b699e7e570258d994c9
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-01-22 12:56:09 +0800

    189
    easy

--- rotateArray/Solution.java
+++ rotateArray/Solution.java
@@ -21,6 +21,6 @@ public static void main(String[] args){
 	int[] a={1,2};
 	new Solution().rotate(a, 0);
 	for(int i=0;i<a.length;i++)
-		System.out.println(a[i]);
+	   System.out.println(a[i]);
 }
 }
