1d1db729d081923767c69fd96b72dfa90771992f
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-04-05 22:21:12 +0800

    796
    easy

---
+++ rotateString/Solution.java
@@ -0,0 +1,11 @@
+package rotateString;
+
+public class Solution {
+	public boolean rotateString(String A, String B) {
+       return (A.length()==B.length()&&(A+A).contains(B));
+    }
+	public static void main(String[] args) {
+		Solution s=new Solution();
+		System.out.println(s.rotateString("abcde", "cdeab"));
+	}
+}
